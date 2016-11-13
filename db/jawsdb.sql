879a6599762e664b2e4eb94fe455411a4cbc09c5
Gael Arrambide <gael_arrambide@hotmail.com>
2016-11-13 09:28:29 -0600

    trying heroku

--- db/jawsdb.sql
+++ db/jawsdb.sql
@@ -1,7 +1,7 @@
 CREATE TABLE fap15sox7l46vvp5.burgers(
 	id INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (id),
-	burger_name VARCHAR(100),
-	devoured BOOLEAN,
+	burger_name VARCHAR(100) NOT NULL,
+	devoured BOOLEAN NOT NULL,
 	createdAt TIMESTAMP NOT NULL,
-	date TIMESTAMP);
+	date TIMESTAMP NOT NULL);
